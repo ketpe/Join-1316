441e91e47b81f9232194b87bfce3376eab7a5874
Peter Ketterlinus <ketpe87@gmail.com>
2025-08-06 20:59:59 +0200

    Added icons for navbar Created navbar-desktop.html Created -navbar-desktop.css Added colors

---
+++ script.js
@@ -0,0 +1,22 @@
+function init() {
+    includeHtml("navbar", "navbar-desktop.html");
+}
+
+function includeHtml(target, file) {
+    let element = document.getElementById(target);
+
+    if (file) {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    element.innerHTML = xhr.responseText;
+                } else {
+                    element.innerHTML = "Error loading file: " + file;
+                }
+            }
+        };
+        xhr.open("GET", file, true);
+        xhr.send();
+    }
+}
