a5dd0dfd01186aa8d0a673a7557f1c2f6c5a0b63
Peter Ketterlinus <ketpe87@gmail.com>
2025-08-16 21:35:30 +0200

    feat: Add add-contact dialog and related functionality with styling

---
+++ scripts/add-contact-dialog.js
@@ -0,0 +1,39 @@
+function onAddContactDialogOpen() {
+    toggleScrollOnBody();
+    addDialogShowClass();
+    document.getElementById('add-contact-dialog').showModal();
+    renderAddContactIntoDialog();
+}
+
+function addContactDialogClose(event) {
+    const dialog = document.getElementById('add-contact-dialog');
+    if (event.target == dialog) {
+        addDialogHideClass();
+        setTimeout(function () {
+            dialog.close();
+            toggleScrollOnBody();
+        }, 1000);
+
+    }
+}
+
+function toggleScrollOnBody() {
+    document.getElementsByTagName('body')[0].classList.toggle('dialog-open');
+}
+
+
+function renderAddContactIntoDialog() {
+    includeHtml("dialog-content-contacts", "add-contact.html");
+}
+
+function addDialogShowClass() {
+    let dialog = document.getElementById('add-contact-dialog');
+    dialog.classList.remove('dialog-hide');
+    dialog.classList.add('dialog-show');
+}
+
+function addDialogHideClass() {
+    let dialog = document.getElementById('add-contacts-dialog');
+    dialog.classList.remove('dialog-show');
+    dialog.classList.add('dialog-hide');
+}
