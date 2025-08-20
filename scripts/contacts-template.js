1b15b0e9c2d54f16f00233023b5ec47dec9364e4
Peter Ketterlinus <ketpe87@gmail.com>
2025-08-20 21:43:28 +0200

    feat: Add JSDoc comments for contact list header and content templates

--- scripts/contacts-template.js
+++ scripts/contacts-template.js
@@ -1,8 +1,22 @@
+/**
+ * Creates the header template for a letter in the contact list
+ * @param {string} firstLetter - The initial letter of the contacts
+ * @returns {string} HTML template for the letter header
+ */
 function getContactListHeaderTemplate(firstLetter) {
     return `<li class="contact-letter">${firstLetter}</li>
                                     <li class="separator-contact-list"></li>`
 };
 
+/**
+ * Creates the HTML template for a single contact
+ * @param {Object} obj - The contact object
+ * @param {string} obj.initial - The initials of the contact
+ * @param {string} obj.firstname - The first name of the contact
+ * @param {string} obj.lastname - The last name of the contact
+ * @param {string} obj.email - The email address of the contact
+ * @returns {string} HTML template for the contact
+ */
 function getContactListContent(obj) {
     return `<li class="contact-item">
                                         <div class="contact-initials">
