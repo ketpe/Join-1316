844153de51980e3f3802f128260154b0e9c990fa
Enrico Hoffmann <enricohoffmann@web.de>
2025-08-24 11:04:05 +0200

    feat: Replace contact option with button for improved interaction in add task form

--- scripts/template.js
+++ scripts/template.js
@@ -1,8 +1,11 @@
 function addTaskContactOption(contact){
     return `
-       <option value="${contact['id']}">
-            <p>${contact['firstname']}</p>
-       </option>
-    
+      <button id="uuid-from-contact" type="button" class="contact-list-btn" active="false" onclick="contactButtonOnListSelect(this)">
+                            <div class="contact-profil-container">
+                                <div class="contact-ellipse cyan"><span>SM</span></div>
+                                <p>Solfa Müller</p>
+                            </div>
+                            <div class="contact-check-icon contact-unchecked" role="img" title="Check or uncheck Icon"></div>
+                        </button>
     `;
 }
