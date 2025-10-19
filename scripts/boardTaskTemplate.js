b5c6248564851bcd7f7ec44f5d7867b7cfb47c6c
Peter Ketterlinus <ketpe87@gmail.com>
2025-10-19 17:40:29 +0200

    Adds mobile swap menu UI to board tasks
    Introduces a mobile-friendly swap menu button and associated styles for board tasks, including a new SVG icon. Enhances usability on mobile devices by enabling easier task status changes through the UI.

--- scripts/boardTaskTemplate.js
+++ scripts/boardTaskTemplate.js
@@ -1,61 +1,39 @@
 
-
-/* function boardTasksTemplate(task, rendererdContacts) {
-    return `<div class="kanban-task" draggable="true" id="${task.id}" ondragstart="startDrag(event, this)" ondragend="endDrag(this)"
-                onclick="openDialog('detail-view-task-dialog',renderDetailViewTemplate('${task.id}'))">
-        <div class="board-task-content">
-            <div class="board-task-head-category ${task.categoryData.categoryColor}">
-                <p>${task.categoryData.title}</p>
-            </div>
-            <div class="board-task-title">
-                <p>${task.title}</p>
-            </div>
-            <div class="board-task-description">
-                <p>${task.description}</p>
-            </div>
-            <div class="board-task-progress">
-                <label for="sub-task-progress"></label>
-                <progress class="sub-task-progressbar" id="sub-task-progress" value="${task.countTrueSubtasks}" max="${task.subTasks.length}"
-                    aria-describedby="progress-value">${task.countTrueSubtasks}</progress>
-                <output class="progress-value-Text" id="progress-value">${task.countTrueSubtasks}/${task.subTasks.length} Subtasks</output>
-            </div>
-            <div class="board-task-assigned-priority">
-                ${rendererdContacts}
-                <div class="board-task-priority">
-                    <div role="img" aria-label="Priority:${task.priority} " class="priority-icon-${task.priority}"></div>
-                </div>
-            </div>
-        </div>
-    </div>`
-} */
-
 function boardTasksTemplate(task, rendererdContacts) {
     return `<div class="kanban-task" draggable="true" id="${task.id}" ondragstart="startDrag(event, this)" ondragend="endDrag(this)"
-                onclick="getDetailViewTask('${task.id}')">
-        <div class="board-task-content">
+    onclick="getDetailViewTask('${task.id}')">
+    <div class="board-task-content">
+        <div class="board-category-and-swap">
             <div class="board-task-head-category ${task.categoryData.categoryColor}">
                 <p>${task.categoryData.title}</p>
             </div>
-            <div class="board-task-title">
-                <p>${task.title}</p>
-            </div>
-            <div class="board-task-description">
-                <p>${task.description}</p>
-            </div>
-            <div class="board-task-progress">
-                <label for="sub-task-progress"></label>
-                <progress class="sub-task-progressbar" id="sub-task-progress" value="${task.countTrueSubtasks}" max="${task.subTasks.length}"
-                    aria-describedby="progress-value">${task.countTrueSubtasks}</progress>
-                <output class="progress-value-Text" id="progress-value">${task.countTrueSubtasks}/${task.subTasks.length} Subtasks</output>
-            </div>
-            <div class="board-task-assigned-priority">
-                ${rendererdContacts}
-                <div class="board-task-priority">
-                    <div role="img" aria-label="Priority:${task.priority} " class="priority-icon-${task.priority}"></div>
-                </div>
+            <button type="button" class="swap-mobile-menu" id="swap-mobile-btn-${task.id}">
+                <div role="img" aria-label="swap Button icon" class="swap-mobile-icon"></div>
+            </button>
+
+        </div>
+
+        <div class="board-task-title">
+            <p>${task.title}</p>
+        </div>
+        <div class="board-task-description">
+            <p>${task.description}</p>
+        </div>
+        <div class="board-task-progress">
+            <label for="sub-task-progress"></label>
+            <progress class="sub-task-progressbar" id="sub-task-progress" value="${task.countTrueSubtasks}"
+                max="${task.subTasks.length}" aria-describedby="progress-value">${task.countTrueSubtasks}</progress>
+            <output class="progress-value-Text" id="progress-value">${task.countTrueSubtasks}/${task.subTasks.length}
+                Subtasks</output>
+        </div>
+        <div class="board-task-assigned-priority">
+            ${rendererdContacts}
+            <div class="board-task-priority">
+                <div role="img" aria-label="Priority:${task.priority} " class="priority-icon-${task.priority}"></div>
             </div>
         </div>
-    </div>`
+    </div>
+</div>`
 }
 
 function boardTaskEmptyTemplate(category) {
@@ -78,3 +56,14 @@ function getAllAssignedContactsTemplate(assignedContacts) {
                     </div>
                 </div>`
 }
+/* Mobile Swap Menu Template
+
+<div class="swap-menu">
+    <button type="button" class="btn-swap-menu" onclick=''>
+        <p>to-do</p>
+    </button>
+    <button type="button" class="btn-swap-menu" onclick='navigateToLegalNotice(event)'>
+        <p>Review</p>
+    </button>
+</div>
+ */
